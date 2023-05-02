7366e50f26ab19893d808dd8c4cd69129d3d2bbc
Adrian Obregon <badrian7898@hotmail.com>
2023-05-02 15:56:44 -0600

    Segundo commit - librerias agregadas

---
+++ InnovaCatalog.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InnovaCatalog.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
