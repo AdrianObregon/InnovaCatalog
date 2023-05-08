cb219ed5ec396cb92acdc8355d31fa4e2911af14
Adrian Obregon <badrian7898@hotmail.com>
2023-05-08 12:08:54 -0600

    Running Identity Server

---
+++ InnovaCatalog.Server.Identity/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using InnovaCatalog.Server.Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace InnovaCatalog.Server.Identity.DbContexts
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+
+
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+
+
+    }
+}
---
+++ InnovaCatalog.Server.Identity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InnovaCatalog.Server.Identity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
